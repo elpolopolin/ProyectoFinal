956bd08d7e5a53e58f279f981758634578659ec9
tomas <polonskytomas@gmail.com>
2023-06-16 19:09:02 -0300

    sid

--- FrontEnd/src/components/Eventos.js
+++ FrontEnd/src/components/Eventos.js
@@ -4,160 +4,162 @@ import MostrarEvento from "./MostrarEvento";
 import axios from "axios";
 
 function Eventos({ eventos }) {
-  let numColumns = 2; // Número de columnas predeterminado
-  const [searchTerm, setSearchTerm] = useState("");
-  if (eventos.length <= 2) {
-    numColumns = eventos.length; // Mostrar 2 eventos en una fila
-  }
-  const [mostrarEvento, setMostrarEvento] = useState(false);
-  const [eventoMostrar, setEventoMostrar] = useState({});
-  const [participantes, setParticipantes] = useState({});
-  
-
-  const handleClick = (evento) => {
-    setMostrarEvento(true);
-    setEventoMostrar(evento);
-  };
-
-  const handleClick2 = () => {
-    setMostrarEvento(false);
-  };
-
-  const cargarParticipantes = (idEvento) => {
-    let link = "http://localhost:3000/getById/";
-    link += idEvento;
-    axios
-      .get(link)
-      .then((result) => {
-        const participantesEvento = result.data;
-        setParticipantes((prevParticipantes) => ({
-          ...prevParticipantes,
-          [idEvento]: participantesEvento,
-        }));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  useEffect(() => {
-    eventos.forEach((evento) => {
-      cargarParticipantes(evento.Id);
-    });
-  }, [eventos]);
-
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
+      let numColumns = 2; // Número de columnas predeterminado
+      const [searchTerm, setSearchTerm] = useState("");
+      if (eventos.length <= 2) {
+        numColumns = eventos.length; // Mostrar 2 eventos en una fila
+      }
+      const [mostrarEvento, setMostrarEvento] = useState(false);
+      const [eventoMostrar, setEventoMostrar] = useState({});
+      const [participantes, setParticipantes] = useState({});
+      
     
-  };
 
-  const filteredEventos = eventos.filter((evento) => {
-    return evento.Nombre.toLowerCase().includes(searchTerm.toLowerCase());
-  });
+      const handleClick = (evento) => {
+        setMostrarEvento(true);
+        setEventoMostrar(evento);
+      };
 
-  return (
-    <div>
-      {!mostrarEvento && (
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={handleSearch}
-          placeholder="Buscar eventos por nombre"
-        />
-      )}
+      const handleClick2 = () => {
+        setMostrarEvento(false);
+      };
 
-      {!mostrarEvento && (
-        <div className="eventos-container">
-          <div
-            id="eventos"
-            className="row container"
-            style={{
-              display: "grid",
-              gridTemplateColumns: `repeat(${numColumns}, 0.5fr)`,
-              gap: "1rem",
-              height: "680px",
-              overflowY: "scroll",
-            }}
-          >
-            {filteredEventos.map((evento) => {
-              let privacidad = "";
-              let direccion = "";
 
-              if (evento.Publico === false) {
-                privacidad = "Solo Invitación";
-                direccion = "Privada";
-              } else {
-                if (evento.Precio != null) {
-                  privacidad = evento.Precio + "$";
-                  direccion = evento.Direccion;
-                } else {
-                  privacidad = "Gratis";
-                  direccion = evento.Direccion;
-                }
-              }
+      const cargarParticipantes = (idEvento) => {
+        let link = "http://localhost:3000/getById/";
+        link += idEvento;
+        axios
+          .get(link)
+          .then((result) => {
+            const participantesEvento = result.data;
+            setParticipantes((prevParticipantes) => ({
+              ...prevParticipantes,
+              [idEvento]: participantesEvento,
+            }));
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
 
-              const participantesEvento = participantes[evento.Id] || [];
+      useEffect(() => {
+        eventos.forEach((evento) => {
+          cargarParticipantes(evento.Id);
+        });
+      }, [eventos]);
 
-              return (
-                <div>
-                  <div 
-                     key={evento.Id}
-                     className="card mb-4"
-                     onClick={() => handleClick(evento)}
-                     
-                  >
-                    <img
-                      src={evento.ImagenEvento}
-                      className="card-img-top ink"
-                      alt="..."
-                    />
-                    <div className="card-body">
-                      <h4 className="card-title">{evento.Nombre}</h4>
-                      <p className="card-text card-text-line">
-                        {format(
-                          new Date(evento.Fecha),
-                          "dd 'de' MMMM 'de' yyyy ' ' HH'hs'"
-                        )}
-                      </p>
-                      <p className="card-text card-text-line">{privacidad}</p>
-                      <p className="card-text">{direccion}</p>
+      const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+        
+      };
+
+      const filteredEventos = eventos.filter((evento) => {
+        return evento.Nombre.toLowerCase().includes(searchTerm.toLowerCase());
+      });
+
+      return (
+        <div>
+          {!mostrarEvento && (
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={handleSearch}
+              placeholder="Buscar eventos por nombre"
+            />
+          )}
+
+          {!mostrarEvento && (
+            <div className="eventos-container">
+              <div
+                id="eventos"
+                className="row container"
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: `repeat(${numColumns}, 0.5fr)`,
+                  gap: "1rem",
+                  height: "680px",
+                  overflowY: "scroll",
+                }}
+              >
+                {filteredEventos.map((evento) => {
+                  let privacidad = "";
+                  let direccion = "";
+
+                  if (evento.Publico === false) {
+                    privacidad = "Solo Invitación";
+                    direccion = "Privada";
+                  } else {
+                    if (evento.Precio != null) {
+                      privacidad = evento.Precio + "$";
+                      direccion = evento.Direccion;
+                    } else {
+                      privacidad = "Gratis";
+                      direccion = evento.Direccion;
+                    }
+                  }
+                  const participantesEvento = participantes[evento.Id] || []; //participantes de cada evento por id jojo
+
+                  return (
+                    <div>
+                      <div 
+                        key={evento.Id}
+                        className="card mb-4"
+                        onClick={() => handleClick(evento)}
+                        
+                      >
+                        <img
+                          src={evento.ImagenEvento}
+                          className="card-img-top ink"
+                          alt="..."
+                        />
+                        <div className="card-body">
+                          <h4 className="card-title">{evento.Nombre}</h4>
+                          <p className="card-text card-text-line">
+                            {format(
+                              new Date(evento.Fecha),
+                              "dd 'de' MMMM 'de' yyyy ' ' HH'hs'"
+                            )}
+                          </p>
+                          <p className="card-text card-text-line">{privacidad}</p>
+                          <p className="card-text">{direccion}</p>
+                        </div>
+                      </div>
+
+                      {
+                      participantesEvento.map((participante) => {
+                          // console.log(participante);
+                          if (participante.IdEvento === evento.Id) {
+                            return (
+                              <img
+                                key={participante.IdUsuario}
+                                src={participante.FotoPerfil}
+                                alt="Participante"
+                                className="foto-perfil-cards"
+                              />
+                            );
+                          }
+                          return null;
+                        })}
+
                     </div>
-                  </div>
-
-                  {
-                  participantesEvento.map((participante) => {
-                      // console.log(participante);
-                      if (participante.IdEvento === evento.Id) {
-                        return (
-                          <img
-                            key={participante.IdUsuario}
-                            src={participante.FotoPerfil}
-                            alt="Participante"
-                            className="foto-perfil-cards"
-                          />
-                        );
-                      }
-                      return null;
-                    })}
-                </div>
-              );
-            })}
+                  );
+                })}
+              </div>
+            </div>
+          )}
+          {mostrarEvento && (
+            <div className="container">
+            
+              <button onClick={handleClick2} className="btn-transparent">
+                &lt;
+              </button>
+            
+              <MostrarEvento evento={eventoMostrar} />
+            
           </div>
+          )}
         </div>
-      )}
-      {mostrarEvento && (
-        <div className="container">
-        
-          <button onClick={handleClick2} className="btn-transparent">
-            &lt;
-          </button>
-         
-          <MostrarEvento evento={eventoMostrar} />
-        
-      </div>
-      )}
-    </div>
-  );
-}
+      );
+    }
 
 export default Eventos;
--- FrontEnd/src/components/LogIn.js
+++ FrontEnd/src/components/LogIn.js
@@ -1,37 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "../icons/Logo.png";
+import RegisterModal from "./RegisterModal";
 
 function LogIn({ username, password, setUsername, setPassword, onLogin, incorrecto }) {
+  const [showRegisterModal, setShowRegisterModal] = useState(false);
+
   const handleLogin = () => {
-    // Aquí puedes agregar tu lógica de autenticación real usando 'username' y 'password'
-    // Por simplicidad, este ejemplo solo llama a la función 'onLogin' cuando se hace clic en el botón
     onLogin();
   };
-
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
   };
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
+  const handleRegisterClick = () => {
+    setShowRegisterModal(true);
+  };
+  const closeRegisterModal = () => {
+    setShowRegisterModal(false);
+  };
 
   return (
     <div className="container">
-      <input
-       type="text"
-       value={username}
-       onChange={handleUsernameChange}
-       placeholder="Nombre de usuario"
-        
-      />
-    
-      <input
-        type="password"
-        value={password}
-        onChange={handlePasswordChange}
-        placeholder="Contraseña"
-      />
-      <button onClick={handleLogin}>Iniciar sesión</button>
-     <p className="text-danger">{incorrecto}</p>
+      <center>
+        <div className="lej">
+          <img src={Logo} className="Logo" alt="Logo" />
+
+          <input
+            type="text"
+            value={username}
+            className="inputt"
+            onChange={handleUsernameChange}
+            placeholder="Nombre de usuario"
+          />
+          <br />
+          <input
+            type="password"
+            value={password}
+            className="inputt"
+            onChange={handlePasswordChange}
+            placeholder="Contraseña"
+          />
+        </div>
+        <br />
+        <button onClick={handleLogin}>Iniciar sesión</button>
+        <p className="text-danger">{incorrecto}</p>
+        <br />
+        <p className="text-white">
+          ¿Aún no tienes una cuenta?{" "}
+          <a className="underline" onClick={() => handleRegisterClick()}>
+            Registrarte
+          </a>
+        </p>
+      </center>
+
+      {showRegisterModal && 
+      <RegisterModal closeModal={closeRegisterModal} />}
     </div>
   );
 }
