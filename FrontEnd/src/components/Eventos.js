4250139ad34ee00d417064399e92a13a27c92269
ROCCO BOLAN <47126613@ort>
2023-06-16 12:05:20 -0300

    cambiospolo

--- FrontEnd/src/components/Eventos.js
+++ FrontEnd/src/components/Eventos.js
@@ -1,28 +1,54 @@
 import React, { useState, useEffect } from "react";
-import { format } from 'date-fns';
+import { format } from "date-fns";
 import MostrarEvento from "./MostrarEvento";
+import axios from "axios";
 
 function Eventos({ eventos }) {
-  let numColumns = 3; // Número de columnas predeterminado
+  let numColumns = 2; // Número de columnas predeterminado
   const [searchTerm, setSearchTerm] = useState("");
   if (eventos.length <= 2) {
     numColumns = eventos.length; // Mostrar 2 eventos en una fila
   }
   const [mostrarEvento, setMostrarEvento] = useState(false);
   const [eventoMostrar, setEventoMostrar] = useState({});
+  const [participantes, setParticipantes] = useState({});
+  
 
   const handleClick = (evento) => {
     setMostrarEvento(true);
     setEventoMostrar(evento);
   };
 
-  const handleClick2 = (evento) => {
+  const handleClick2 = () => {
     setMostrarEvento(false);
-    
   };
+
+  const cargarParticipantes = (idEvento) => {
+    let link = "http://localhost:3000/getById/";
+    link += idEvento;
+    axios
+      .get(link)
+      .then((result) => {
+        const participantesEvento = result.data;
+        setParticipantes((prevParticipantes) => ({
+          ...prevParticipantes,
+          [idEvento]: participantesEvento,
+        }));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    eventos.forEach((evento) => {
+      cargarParticipantes(evento.Id);
+    });
+  }, [eventos]);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+    
   };
 
   const filteredEventos = eventos.filter((evento) => {
@@ -33,79 +59,98 @@ function Eventos({ eventos }) {
     <div>
       {!mostrarEvento && (
         <input
-        type="text"
-        value={searchTerm}
-        onChange={handleSearch}
-        placeholder="Buscar eventos por nombre"
-      />
-      
-
+          type="text"
+          value={searchTerm}
+          onChange={handleSearch}
+          placeholder="Buscar eventos por nombre"
+        />
       )}
-          
+
       {!mostrarEvento && (
-         <div className="eventos-container">
-        <div
-          id="eventos"
-          className="row container"
-          style={{
-            display: "grid",
-            gridTemplateColumns: `repeat(${numColumns}, 1fr)`,
-            gap: "1rem",
-            height: '680px',
-            overflowY: "scroll",
-          }}
-        >
-            
+        <div className="eventos-container">
+          <div
+            id="eventos"
+            className="row container"
+            style={{
+              display: "grid",
+              gridTemplateColumns: `repeat(${numColumns}, 0.5fr)`,
+              gap: "1rem",
+              height: "680px",
+              overflowY: "scroll",
+            }}
+          >
+            {filteredEventos.map((evento) => {
+              let privacidad = "";
+              let direccion = "";
 
-          {filteredEventos.map((evento) => {
-            let privacidad = "";
-            let direccion = "";
-
-            if (evento.Publico === false) {
-              privacidad = 'Solo Invitación';
-              direccion = "Privada";
-            } else {
-              if (evento.Precio != null) {
-                privacidad = evento.Precio + "$";
-                direccion = evento.Direccion;
+              if (evento.Publico === false) {
+                privacidad = "Solo Invitación";
+                direccion = "Privada";
               } else {
-                privacidad = "Gratis";
-                direccion = evento.Direccion;
+                if (evento.Precio != null) {
+                  privacidad = evento.Precio + "$";
+                  direccion = evento.Direccion;
+                } else {
+                  privacidad = "Gratis";
+                  direccion = evento.Direccion;
+                }
               }
-            }
 
-            return (
-              <div key={evento.Id} className="" onClick={() => handleClick(evento)}>
-                <div className="card mb-4">
-                  <img
-                    src={evento.ImagenEvento}
-                    className="card-img-top ink"
-                    alt="..."
-                  />
-                  <div className="card-body">
-                    <h4 className="card-title">{evento.Nombre}</h4>
-                    <p className="card-text card-text-line">
-                      {format(
-                        new Date(evento.Fecha),
-                        "dd 'de' MMMM 'de' yyyy ' ' HH'hs'"
-                      )}
-                    </p>
-                    <p className="card-text card-text-line">{privacidad}</p>
-                    <p className="card-text">{direccion}</p>
+              const participantesEvento = participantes[evento.Id] || [];
+
+              return (
+                <div>
+                  <div 
+                     key={evento.Id}
+                     className="card mb-4"
+                     onClick={() => handleClick(evento)}
+                     
+                  >
+                    <img
+                      src={evento.ImagenEvento}
+                      className="card-img-top ink"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h4 className="card-title">{evento.Nombre}</h4>
+                      <p className="card-text card-text-line">
+                        {format(
+                          new Date(evento.Fecha),
+                          "dd 'de' MMMM 'de' yyyy ' ' HH'hs'"
+                        )}
+                      </p>
+                      <p className="card-text card-text-line">{privacidad}</p>
+                      <p className="card-text">{direccion}</p>
+                    </div>
                   </div>
-                </div>
-              </div>
-              
-            );
-          })}
-          </div>
 
+                  {
+                  participantesEvento.map((participante) => {
+                      // console.log(participante);
+                      if (participante.IdEvento === evento.Id) {
+                        return (
+                          <img
+                            key={participante.IdUsuario}
+                            src={participante.FotoPerfil}
+                            alt="Participante"
+                            className="foto-perfil-cards"
+                          />
+                        );
+                      }
+                      return null;
+                    })}
+                </div>
+              );
+            })}
+          </div>
         </div>
       )}
       {mostrarEvento && (
         <div className="container">
-        <MostrarEvento evento={eventoMostrar} />
-        <button onClick={handleClick2} className="btn btn-primary">Regresar</button>
+          <MostrarEvento evento={eventoMostrar} />
+          <button onClick={handleClick2} className="btn btn-primary">
+            Regresar
+          </button>
         </div>
       )}
     </div>
