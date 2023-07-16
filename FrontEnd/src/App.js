fd4603053ee5e03740cd2526a95d857090614655
tomas <polonskytomas@gmail.com>
2023-07-15 23:39:07 -0300

    verificacion usuario loggeado

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -10,34 +10,38 @@ import Profile from "./components/Profile";
 import Calendar from "./components/Calendar";
 import Entradas from "./components/Entradas";
 
-
 export const AuthContext = createContext();
 
 function App() {
   const [eventos, setEventos] = useState([]);
   const [usuarios, setUsuarios] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [body, setBody] = useState({ username: '', password: '' })
+  const [body, setBody] = useState(() => {
+    const storedBody = localStorage.getItem('body');
+    return storedBody ? JSON.parse(storedBody) : { username: '', password: '' };
+  });
   const [incorrecto, setIncorrecto] = useState("");
   const [userLogged, setUserLogged] = useState({});
 
   useEffect(() => {
-   
     cargarEventos();
-    const token = localStorage.getItem('auth');// Verificar si existe un token de autenticación en el almacenamiento local
-    const userJson = localStorage.getItem('userLogged');
-    const user = JSON.parse(userJson);
 
-    if (token && user != null) {
-      setIsLoggedIn(true);
-      setUserLogged(user);
-    }
-
-    //localStorage.clear();
-    
-    
-  }, []);
-
+      const token = localStorage.getItem('auth');
+      const userJson = localStorage.getItem('userLogged');
+     
+        if (token && userJson != null) {
+          const user = JSON.parse(userJson);
+          setIsLoggedIn(true);
+          cargarUsuarioId(user);
+   
+        } else {
+          setIsLoggedIn(false);
+          setUserLogged({});
+        }
+      
+        checkPasswordChange();
+       //localStorage.clear();
+    }, []);
 
   const cargarEventos = () => {
     axios
@@ -49,7 +53,6 @@ function App() {
       .catch((error) => {
         console.log(error);
       });
-     
   };
 
   const cargarUsuarios = () => {
@@ -64,111 +67,142 @@ function App() {
       });
   };
 
+  const cargarUsuarioId = async (user) => {
+    const link = "http://localhost:3000/usuarios/getbyid/" + user.Id;
+    console.log(link);
+    axios
+      .get(link)
+      .then((result) => {
+        setUserLogged(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const inputChange = ({ target }) => {
-    const { name, value } = target
+    const { name, value } = target;
     setBody({
-        ...body,
-        [name]: value
-    }) }
+      ...body,
+      [name]: value,
+    });
+  };
 
-    const onSubmit = (event) => {
-      event.preventDefault();
-      axios.post('http://localhost:3000/usuarios/login', body)
+  const onSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:3000/usuarios/login', body)
       .then((result) => {
-       
-      localStorage.setItem('auth', 'yes'); // Guardar el token en el almacenamiento local
-      localStorage.setItem('userLogged', JSON.stringify(result.data))
-      window.location.reload(false)
+        const user = result.data;
+        setIsLoggedIn(true);
+        setUserLogged(user);
+        localStorage.setItem('auth', 'yes');
+        localStorage.setItem('userLogged', JSON.stringify(user));
+        localStorage.setItem('body', JSON.stringify(body));
       })
       .catch(({ response }) => {
-        setIncorrecto("Usuario o contraseña incorrecto")
-      })
-    }
+        setIncorrecto("Usuario o contraseña incorrecto");
+      });
+  };
 
-   
+  const logout = () => {
+    setIsLoggedIn(false);
+    setUserLogged({});
+    localStorage.removeItem('auth');
+    localStorage.removeItem('userLogged');
+    localStorage.removeItem('body');
+  };
+
+  const checkPasswordChange = () => {
+    const storedBody = localStorage.getItem('body');
+    if (storedBody) {
+        const storedCredentials = JSON.parse(storedBody);
+        axios.post('http://localhost:3000/usuarios/login', body)
+        .then((result) => {
+
+        
+      })
+      .catch(({ response }) => {
+        logout();
+        window.location.reload(false)
+      });
+
+      }
+
+    }
   
+
+
 
   return (
     <BrowserRouter>
-     <Routes>
-            <Route path="/Registrarse" element={<Registrarse />} />
-        </Routes>
-      <AuthContext.Provider value={{ isLoggedIn, userLogged }}>
-     
-        <div className="bg-[#252525] w-full min-h-screen font-sans justify-items-center">
-         
-        {!isLoggedIn &&  (
-            <div className="logIn  p-10 ">
-              <center>
+    <Routes>
+      <Route path="/Registrarse" element={<Registrarse />} />
+    </Routes>
+    <AuthContext.Provider value={{ isLoggedIn, userLogged }}>
+      <div className="bg-[#252525] w-full min-h-screen font-sans justify-items-center">
+        {!isLoggedIn && (
+          <div className="logIn p-10">
+            <center>
               <form onSubmit={onSubmit}>
-              <div className="">
-                <br></br>
-                <img src={Logo} className="Logo mt-3" alt="Logo" />
-                <div className="my-4">
-                <input
-                  id="nombreUsuario"
-                  name="username"
-                  type="text"
-                  value={body.username}
-                  className="input input-bordered w-full max-w mb-4 h-9 bg-pink-300 text-white"
-                  onChange={inputChange}
-                  placeholder="Nombre de usuario"
-                />
-                <br />
-                <input
-                  type="password"
-                  name="password"
-                  value={body.password}
-                  className="input input-bordered w-full max-w bg-pink-300 text-white h-9"
-                  onChange={inputChange}
-                  placeholder="Contraseña"
-                />
-              
-              </div>
-              
-              <button className="btn btn-outline  btn-wide text-pink-200" type="submit">
-                Iniciar sesión
-              </button>
-              </div>
-            </form>
-            <p className=" text-pink-500 mt-10 " >
-              {incorrecto}
-            </p>
-            
-          
-            <p className="absolute bottom-0 left-0 right-0 text-pink-200 text-center mb-10 " >
-              ¿Aún no tienes una cuenta?{" "}
-              <Link to="/Registrarse" className="">
-              <a className="underline cursor-pointer">
-                Registrarte
-              </a>
-              </Link>
-            </p>
-           
-        
-        </center>
-            </div>
-          )}
+                <div className="">
+                  <br></br>
+                  <img src={Logo} className="Logo mt-3" alt="Logo" />
+                  <div className="my-4">
+                    <input
+                      id="nombreUsuario"
+                      name="username"
+                      type="text"
+                      value={body.username}
+                      className="input input-bordered w-full max-w mb-4 h-9 bg-pink-300 text-white"
+                      onChange={inputChange}
+                      placeholder="Nombre de usuario"
+                    />
+                    <br />
+                    <input
+                      type="password"
+                      name="password"
+                      value={body.password}
+                      className="input input-bordered w-full max-w bg-pink-300 text-white h-9"
+                      onChange={inputChange}
+                      placeholder="Contraseña"
+                    />
+                  </div>
+                  <button className="btn btn-outline  btn-wide text-pink-200" type="submit">
+                    Iniciar sesión
+                  </button>
+                </div>
+              </form>
+              <p className="text-pink-500 mt-10">
+                {incorrecto}
+              </p>
+              <p className="absolute bottom-0 left-0 right-0 text-pink-200 text-center mb-10">
+                ¿Aún no tienes una cuenta?{" "}
+                <Link to="/Registrarse" className="underline cursor-pointer">
+                  Registrarte
+                </Link>
+              </p>
+            </center>
+          </div>
+        )}
 
-          {isLoggedIn && (
-            <div className="Home">
-              <Routes>
-                <Route path="/" element={<Eventos eventos={eventos} />} />
-                <Route path="/friends" element={<Friends />} />
-                <Route path="/profile" element={<Profile usuario={userLogged}/>} />
-                <Route path="/entradas" element={<Entradas />} />
-                <Route path="/calendar" element={<Calendar />} />
-              </Routes>
+        {isLoggedIn && (
+          <div className="Home">
+            <Routes>
+              <Route path="/" element={<Eventos eventos={eventos} />} />
+              <Route path="/friends" element={<Friends />} />
+              <Route path="/profile" element={<Profile usuario={userLogged} logout={logout}/>} />
+              <Route path="/entradas" element={<Entradas />} />
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
 
-              <div className="bottom-navbar">
-                <NavBar usuario={userLogged} />
-              </div>
+            <div className="bottom-navbar">
+              <NavBar usuario={userLogged} />
             </div>
-          )}
-        </div>
-      </AuthContext.Provider>
-    </BrowserRouter>
+          </div>
+        )}
+      </div>
+    </AuthContext.Provider>
+  </BrowserRouter>
   );
 }
 
--- FrontEnd/src/components/Profile.js
+++ FrontEnd/src/components/Profile.js
@@ -1,76 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { format } from "date-fns";
-import './Profile.css';
+import './styles/Profile.css';
 
-
-function Profile({ usuario}) {
+function Profile({ usuario, logout }) { // Receive logout prop
   const [descripcion, setDescripcion] = useState(false);
 
-
   useEffect(() => {
-    console.log(usuario)
-    if (usuario.Descripcion != ""){
+    if (usuario.Descripcion !== "") {
       setDescripcion(true);
-  }
-  else {
+    } else {
       setDescripcion(false);
-  }
+    }
   }, []);
 
-
   return (
-    <div className="profile-container" >
-      <img src={usuario.FotoPerfil} className="profile-image rounded-full ring-2" alt="Profile" />
-        <h1 className="profile-name col-start-1">
-          {usuario.Nombre} {usuario.Apellido}
-        </h1>
-          <h1 className="profile-username col-start-2">@{usuario.NombreUsuario}</h1>
-           <div className="caja-descripcion mt-2  ">
-              {descripcion && (
-                <div>
-                  <p className="">{usuario.Descripcion}</p>
-            </div>
-                     
-          )}
-          {!descripcion && (
-            <div className="caja-descripcion rounded-md">
-              <p className="profile-descripcion">
-                <a className="underline">Agregar descripción</a>
-              </p>
-            </div>
-          )}
-  </div>
-        
-      <div className="profile-descripcion">
-        <div className="usuario-fechanacimiento">
-        {format(
-                    new Date(usuario.FechaNacimiento),
-                    "MMMM/dd/yyyy "
-                  )}
-        
+    <div className="profile-container">
+      <img src={usuario.FotoPerfil} className="profile-image" alt="Profile" />
+      <h1 className="profile-name">
+        {usuario.Nombre} {usuario.Apellido}
+      </h1>
+      <p className="profile-username">@{usuario.NombreUsuario}</p>
+      
+      {descripcion && (
+        <div className="caja-descripcion">
+          <p className="profile-descripcion">{usuario.Descripcion}</p>
         </div>
-          
-        <p className="mb-2 mt-2 caja-descripcion2">{usuario.estado} </p>
-        <p className="mb-2 mt-2 caja-descripcion2">Intereses: </p>
-        <div className="flex w-full">
-          <div className="grid-intereses h-20 flex-grow card bg-base-0 rounded-box place-items-center">{usuario.interes1}</div>
-          <div className="divider-horizontal"></div>
-          <div className="grid-intereses flex-grow card bg-base-0 rounded-box place-items-center">{usuario.interes1}</div>
-          <div className="divider divider-horizontal"></div>
-          <div className="grid-intereses h-20 flex-grow card bg-base-0 rounded-box place-items-center">{usuario.interes1}</div>
+      )}
+      {!descripcion && (
+        <div className="caja-descripcion">
+          <p className="profile-descripcion">
+            <a className="underline">Agregar descripcion</a>
+          </p>
         </div>
+      )}
 
-        <div className="flex joinButtons">
-          <button className="btn join-item">MIS EVENTOS +</button>
-          <button className="btn join-item">EVENTOS ASISTIDOS</button>
-        </div>
-      </div>
- 
+      <br></br>
+      <button onClick={logout} className="logout-button btn btn-outline btn-error">
+        Cerrar Sesion
+      </button>
     </div>
-
   );
 }
 
 export default Profile;
-
-
