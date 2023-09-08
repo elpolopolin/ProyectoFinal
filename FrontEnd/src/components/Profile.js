96c146ab6bfd9d33d8c21437fc68dc28cf2a5793
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-08 12:13:19 -0300

    cambios user view

--- FrontEnd/src/components/Profile.js
+++ FrontEnd/src/components/Profile.js
@@ -2,11 +2,14 @@ import React, { useContext ,useEffect, useState } from "react";
 import { format } from "date-fns";
 import './styles/Profile.css';
 import { UsuarioContext } from "../App";
+import dayjs from "dayjs";
 
 function Profile({ logout }) { // Receive logout prop
 
   const [descripcion, setDescripcion] = useState(false);
   const usuario = useContext(UsuarioContext); //llama a usuario del context, este puede ser llamado desde toda la app..
+
+  dayjs(usuario.FechaNacimiento).format("dd MM yyyy");
 
   useEffect(() => {
     if (usuario.Descripcion !== "") {
@@ -23,12 +26,20 @@ function Profile({ logout }) { // Receive logout prop
         {usuario.Nombre} {usuario.Apellido}
       </h1>
       <p className="profile-username">@{usuario.NombreUsuario}</p>
-      
+      <br></br>
       {descripcion && (
+        <span class="inline-block align-baseline ...">
         <div className="caja-descripcion">
           <p className="profile-descripcion">{usuario.Descripcion}</p>
         </div>
+        </span>
       )}
+
+      {/* <span class="inline-block align-baseline ...">
+        <div className="caja-descripcion">
+          <p className="profile-descripcion">{usuario.Descripcion}</p> intereses
+        </div>
+        </span> */}
       {!descripcion && (
         <div className="caja-descripcion">
           <p className="profile-descripcion">
