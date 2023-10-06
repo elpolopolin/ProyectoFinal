86c561f8438227c0d8853b13b0e4a6ac46df628e
TOMAS POLONSKY <47066838@ort>
2023-10-06 12:00:18 -0300

    6/10/2023

--- FrontEnd/src/components/Categorias.js
+++ FrontEnd/src/components/Categorias.js
@@ -24,19 +24,19 @@ function Categorias() {
     return (
         <div className="">
              <div className="nana-container overflow-y-scroll text-white">
-             <h2 className="font-bold text-2xl text-center py-8">Categorias</h2>
+             <h2 className="font-bold text-2xl text-center py-6">Categorias</h2>
         <div className=""> 
-            <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8 ">
+            <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 sm:py-16 lg:max-w-7xl lg:px-8 " style={{marginTop: "-30px"}}>
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 xsm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {Categorias.map((categoria) => (
             <a key={categoria.IdCategoria} className="group">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                <img
-                  src={categoria.Imagen}
-                  className="h-full w-full object-cover object-center group-hover:opacity-75"
-                />
+            <img
+            src={categoria.Imagen}
+            className="h-40 w-full object-cover object-center group-hover:opacity-75"  // Adjust the height (h-40) as needed
+            />
               </div>
-              <h3 className="mt-4 text-sm ">{categoria.NombreCategoria}</h3>
+              <h3 className="mt-2 text-sm font-bold ">{categoria.NombreCategoria}</h3>
              
             </a>
           ))}
