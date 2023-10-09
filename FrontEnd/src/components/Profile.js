2bf3d81f9a4f292d763a4ea3d2c08b10cb4b671a
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-09 11:48:57 -0300

    participantes de evento

--- FrontEnd/src/components/Profile.js
+++ FrontEnd/src/components/Profile.js
@@ -31,10 +31,7 @@ function Profile({ logout }) { // Receive logout prop
     {/*configuration */}
       <div className="drawer">
     <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-    <div className="drawer-content">
-     
-        
-    </div> 
+    <div className="drawer-content">    </div> 
       <div className="drawer-side">
         <label htmlFor="my-drawer" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
