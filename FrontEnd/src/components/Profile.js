6dfe37974e372013f7a1e2abf60b1e01a407d658
tomas <polonskytomas@gmail.com>
2023-10-12 00:23:31 -0300

    correcion de lo que hizo el salame de rempel

--- FrontEnd/src/components/Profile.js
+++ FrontEnd/src/components/Profile.js
@@ -26,36 +26,60 @@ function Profile({ logout }) { // Receive logout prop
 
 
   return (
-    <div className="profile-container">
-
-    {/*configuration */}
-      <div className="drawer">
-    <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-    <div className="drawer-content">    </div> 
-      <div className="drawer-side">
-        <label htmlFor="my-drawer" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-          <li><a>Editar perfil</a></li>
-          <li><a>Ayuda</a></li>
-          <li> <a className="hover:bg-red-500" onClick={logout}>
-        Cerrar Sesion
-        </a> </li>
-        </ul>
-      </div>
-    </div>
-    <div class="flex justify-center min-h-screen">
-    <div class="h-auto sm:w-96 w-[360px] transition-all text-white rounded-lg overflow-hidden">
-    <div class="h-80 w-full ">
-            <img src={usuario.FotoPerfil}/>
+    <div className="profile-container overflow-y-auto">
+   
+   <div className="drawer">
+        <input
+          id="my-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+          checked={mostrarPanelConfiguracion}
+          onChange={() => setMostrarPanelConfiguracion(!mostrarPanelConfiguracion)}
+        />
+        <div className="drawer-content"></div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            <li><a>Editar perfil</a></li>
+            <li><a>Mis Eventos</a></li>
+            <li>
+              <a className="hover:bg-red-500" onClick={logout}>
+                Cerrar Sesión
+              </a>
+            </li>
+          </ul>
         </div>
-        <div class="md:-mt-12 -mt-24 px-4 ">
-            <p class="text-2xl font-semibold"></p>
-            <div class="flex gap-6 ">
-                <p class="text-lg font-semibold" >{usuario.Nombre} {usuario.Apellido} </p>
+      </div>
+
+   
+    <div class="">
+
+     <i class="text-2xl p-2 absolute top-0 right-2 text-black fa fa-cog" aria-hidden="true"  onClick={() => setMostrarPanelConfiguracion(!mostrarPanelConfiguracion)}></i>
+  
+    <div class="  transition-all text-white rounded-lg ">
+    <div class="  ">
+            <img src={usuario.FotoPerfil} style={{width: "100%", height:"300px"}}/>
+        </div>
+        <div class="md:mt-12 mt-12  ">
+                
                 <div class="flex gap-1">
-                    <p class="text-lg font-semibold">@{usuario.NombreUsuario}</p>
+                    <p class="text-lg font-bold">@{usuario.NombreUsuario}</p>
                 </div>
+          
+            <div class="flex gap-20 mt-5 text-m">
+            <div class="flex flex-col gap-1">
+                <div class="flex gap-3 items-center text-gray-200">
+                    <i class="text-gray-100 fa fa-id-card-o"></i>
+                    <p class=" font-semibold" >{usuario.Nombre} {usuario.Apellido} </p>
+                </div>
+                
+                <div class="flex gap-3 items-center">
+                    <i class="text-gray-100 fa fa-address-card"></i>
+                    <p class="font-semibold ">{usuario.Genero}</p>
+                </div>
+               
             </div>
+
             <div class="flex flex-col gap-1">
                 <div class="flex gap-3 items-center text-gray-200">
                     <i class="text-gray-100 fa fa-id-card-o"></i>
@@ -65,19 +89,27 @@ function Profile({ logout }) { // Receive logout prop
                     <i class="text-gray-100 fa fa-location-arrow"></i>
                     <p class="font-semibold ">{usuario.Direccion}</p>
                 </div>
-                <div class="flex gap-3 items-center">
-                    <i class="text-gray-100 fa fa-address-card"></i>
-                    <p class="font-semibold ">{usuario.Genero}</p>
-                </div>
-                <div class="flex gap-3 items-center">
-                    <i class="text-gray-100"></i>
-                    <p class="font-semibold ">{usuario.Descripcion}</p>
-                </div>
+                
             </div>
-            <button class="my-5 h-12 w-full bg-blue-500 cursor-pointer text-white transition-all hover:bg-blue-800 rounded-lg ">Añadir como amigo</button>
+            
+            </div>
+
+                    <div className="border rounded-lg p-2 mt-5 mx-auto max-w-md h-30 overflow-y-auto">
+                    <p className="text-center">{usuario.Descripcion}</p>
+                    </div> 
+            {    /*   <button class="my-5 h-12 w-full bg-blue-500 cursor-pointer text-white transition-all hover:bg-blue-800 rounded-lg ">Añadir como amigo</button>  esto va solo en verperfil*/ }
         </div>
     </div>
 </div>
+
+
+    
+    </div>
+  );
+}
+
+export default Profile;
+
 {/*     
     <img src={usuario.FotoPerfil} className="profile-image" alt="Profile" />
       <h1 className="profile-name">
@@ -110,11 +142,3 @@ function Profile({ logout }) { // Receive logout prop
       </div>
       
       <br></br> */}
-
-
-    
-    </div>
-  );
-}
-
-export default Profile;
